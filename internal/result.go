43a24bd4d220c0926a0dbb8404e9c7ce456a31cf
Alexander Konovalov <shuryak@protonmail.com>
2020-07-25 18:35:17 +0300

    feat: error output function

---
+++ internal/result.go
@@ -0,0 +1,41 @@
+package internal
+
+import (
+	"encoding/json"
+	"github.com/shuryak/shuryak-backend/internal/models"
+	"net/http"
+)
+
+func ErrorWriter(w *http.ResponseWriter, errorCode models.ErrorCode, description string) {
+	var httpStatusCode int
+
+	switch errorCode {
+	case models.BadRequest:
+		httpStatusCode = http.StatusBadRequest
+	case models.InternalError:
+		httpStatusCode = http.StatusInternalServerError
+	case models.BadAuth:
+		httpStatusCode = http.StatusBadRequest
+	case models.NotAuthorized:
+		httpStatusCode = http.StatusUnauthorized
+	case models.InvalidToken:
+		httpStatusCode = http.StatusUnauthorized
+	case models.ExpiredToken:
+		httpStatusCode = http.StatusForbidden
+	case models.NotUniqueData:
+		httpStatusCode = http.StatusBadRequest
+	case models.InvalidFieldLength:
+		httpStatusCode = http.StatusBadRequest
+	default:
+		httpStatusCode = http.StatusInternalServerError
+	}
+
+	(*w).WriteHeader(httpStatusCode)
+
+	errorMessage := models.ErrorDTO{
+		ErrorCode: errorCode,
+		Message:   description,
+	}
+	json.NewEncoder(*w).Encode(errorMessage)
+	return
+}
